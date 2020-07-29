61893713b8d6dc4a6b3a9209bfe9078e67c160af
PC-1 <alganiiqbal@gmail.com>
2020-07-29 08:05:42 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/unikom/contactapp/MainActivity.java
@@ -0,0 +1,103 @@
+package id.ac.unikom.contactapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+import id.ac.unikom.contactapp.adapter.ContactAdapter;
+import id.ac.unikom.contactapp.db.AppDatabase;
+import id.ac.unikom.contactapp.model.Contact;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView list;
+    ContactAdapter contactAdapter;
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i=new Intent(this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        super.onResume();
+        finish();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        list = findViewById(R.id.list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        list.setLayoutManager(layoutManager);
+        list.setHasFixedSize(true);
+
+        // QUERY KE DATABASE
+        List<Contact> lapors = AppDatabase
+                .getInstance(this).contactDao().getAll();
+        // Array lapor mapping ke row_layout
+        contactAdapter = new ContactAdapter(this,lapors);
+        list.setAdapter(contactAdapter);
+
+    }
+    @Override
+    public void onResume() {  // After a pause OR at startup
+        // QUERY KE DATABASE
+        super.onResume();
+        List<Contact> lapors = AppDatabase
+                .getInstance(this).contactDao().getAll();
+        // Array lapor mapping ke row_layout
+        contactAdapter = new ContactAdapter(this, lapors);
+        list.setAdapter(contactAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
